e191daa370949aa9a748b80a7a1ed1ba35db4220
Dharshith Erra <113717382+Dharshith77@users.noreply.github.com>
2025-04-04 12:44:16 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -8,7 +8,8 @@ def home():
     return "Bot is alive!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8081)
+
 
 def keep_alive():
     t = Thread(target=run)
