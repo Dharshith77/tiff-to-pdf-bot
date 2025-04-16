4ed26d5d0e9e4782d06e9dd9c4b37c5408f91f26
Dharshith Erra <113717382+Dharshith77@users.noreply.github.com>
2025-04-16 12:11:02 +0530

    Update TifftoPdf.py

--- TifftoPdf.py
+++ TifftoPdf.py
@@ -9,7 +9,7 @@ from flask import request, Response
 import asyncio
 import nest_asyncio
 import time
-WEBHOOK_URL = 'https://worker-production-bce8.up.railway.app'
+WEBHOOK_URL = 'https://worker-production-bce8.up.railway.app/webhook'
 
 nest_asyncio.apply()
 
